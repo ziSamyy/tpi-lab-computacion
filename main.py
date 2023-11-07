3a127a9ad0924563e33f91aff39565b89a351e82
Samuel Argote <lautaro2002ap@gmail.com>
2023-11-07 00:11:50 -0300

    Funciones de los menus

--- main.py
+++ main.py
@@ -99,3 +99,84 @@ def clientagenda(clientinfo):
             agenda.write(f'Nombre: {clientinfo[1]}\n')
             agenda.write(f'Apellido: {clientinfo[2]}\n')
             agenda.write(f'Telefono: {clientinfo[3]}\n')
+#######################
+# MENUS
+#######################
+def terrain_menu():
+    print('')
+    print('Que unidad desea usar?')
+    print('1. Metros')
+    print('2. Pies')
+    print('3. Yardas') 
+    print('')
+    while True:
+        sistema = int(input('Ingresa la unidad que se usara: '))
+        match sistema:
+            case 1:
+                longitud = float(input("ingrese la longitud del terreno (Metros): "))
+                ancho = float(input("ingrese el ancho del terreno (Metros): "))
+                espesor = float(input("ingrese el espesor del terreno (Metros): "))
+                volumen = (longitud*ancho*espesor)
+                sistema = 'Metros'
+                return sistema, longitud, ancho, espesor, volumen
+            case 2:
+                longitud = float(input("ingrese la longitud del terreno (Pies): "))
+                ancho = float(input("ingrese el ancho del terreno (Pies): "))
+                espesor = float(input("ingrese el espesor del terreno (Pies): "))
+                volumen = (longitud*ancho*espesor)
+                sistema = 'Pies'
+                return sistema, longitud, ancho, espesor, volumen
+            case 3:
+                longitud = float(input("ingrese la longitud del terreno (Yardas): "))
+                ancho = float(input("ingrese el ancho del terreno (Yardas): "))
+                espesor = float(input("ingrese el espesor del terreno (Yardas): "))
+                sistema = 'Yardas'
+                volumen = (longitud*ancho*espesor)
+                return sistema, longitud, ancho, espesor, volumen
+            case _:
+                print('')
+                print('❌Ingrese una opcion valida')
+                print('')
+                continue
+
+def concrete_type_menu():
+    print('')
+    print('Ingresa la dosificacion de Concreto')
+    print("esta puede ser de 5 tipos que varia en precio y resistencia ")
+    print('Tener en cuenta que depende el tipo varia el precio y resistencia.')
+    print('')
+    
+    print('1. 246')
+    print('2. 210')
+    print('3. 175')
+    print('4. 140')
+    print('5. 105')
+    print('')
+    while True:
+        concrete_type = int(input('Ingrese el tipo de concreto que desea usar: '))
+        match concrete_type:
+            case 1:
+                materiales = [420, 0.67, 0.67, 0.22] # Cemento (kg), arena(m3), grava(m3), agua(m3)
+                type = '246'
+                return materiales, type
+            case 2:
+                materiales = [350, 0.56, 0.84, 0.18] # Cemento (kg), arena(m3), grava(m3), agua(m3)
+                type = '210'
+                return materiales, type
+            case 3:
+                materiales = [300, 0.56, 0.96, 0.17] # Cemento (kg), arena(m3), grava(m3), agua(m3)
+                type = '175'
+                return materiales, type
+            case 4:
+                materiales = [260, 0.63, 0.84, 0.17] # Cemento (kg), arena(m3), grava(m3), agua(m3)
+                type = '140'
+                return materiales, type
+            case 5:
+                materiales = [210, 0.5, 1, 0.16]
+                type = '105'
+                return materiales, type
+            case _:
+                print('')
+                print('❌Ingrese una opcion valida')
+                print('')
+                continue
